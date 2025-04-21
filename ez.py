ee9457d8a95c894c4b2e3b8e2d6d2ccd6b9eb90b
Mauricio Estrada De La Garza <pestrm1@clarios.com>
2025-04-21 09:30:02 -0600

    pruebas dotenv

--- ez.py
+++ ez.py
@@ -1,1 +1,12 @@
-print("Hola Mundo")
+from dotenv import load_dotenv
+import os
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+# Acceder a las variables de entorno
+usuario = os.getenv("user")
+contraseña = os.getenv("pass")
+
+print(f"Usuario: {usuario}")
+
--- gg.py
+++ gg.py
@@ -1,18 +1,8 @@
-#import sys
-#print(sys.version)
 
 import subprocess
 import sys
-import os
 
-# Asegúrate de que pip esté actualizado
-subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+# Instalar python-dotenv
+subprocess.check_call([sys.executable, "-m", "pip", "install", "python-dotenv"])
 
-# Instala Jupyter Notebook
-subprocess.check_call([sys.executable, "-m", "pip", "install", "notebook"])
-
-print("¡Instalación completada!")
-
-# Lanza Jupyter Notebook automáticamente
-notebook_dir = os.getcwd()  # Carpeta actual (puedes cambiarla si lo necesitas)
-subprocess.run([sys.executable, "-m", "notebook", notebook_dir])
+print("¡Instalación de dotenv completada!")
