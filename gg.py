0fc728fd06fdc5f97b744e9e83589af16e6409d0
Mauricio Estrada De La Garza <pestrm1@clarios.com>
2025-04-21 09:33:53 -0600

    testing

--- gg.py
+++
@@ -1,8 +0,0 @@
-
-import subprocess
-import sys
-
-# Instalar python-dotenv
-subprocess.check_call([sys.executable, "-m", "pip", "install", "python-dotenv"])
-
-print("¡Instalación de dotenv completada!")
